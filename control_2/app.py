ea7ff2da8fa79ff2723cd4d783a5a119ef33405f
svetlana1212 <svetik.kosareva2004@mail.ru>
2025-01-15 11:38:46 +0300

    Control 2

--- control_2/app.py
+++ control_2/app.py
@@ -1,56 +1,77 @@
 import sqlite3
+
+# для создания веб-приложения, работы с запросами и обработки исключений HTTP
 from fastapi import FastAPI, Request, HTTPException
+
+# для возвращения HTML и JSON ответов
 from fastapi.responses import HTMLResponse, JSONResponse
+
+# для работы с HTML-шаблонами
 from fastapi.templating import Jinja2Templates
+
+# для работы со статическими файлами
 from fastapi.staticfiles import StaticFiles
+
+# для работы с ORM и подключением к базе данных
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from fastapi import Request
 
 
-# FastAPI app setup
+# Создаем экземпляр приложения FastAPI
 app = FastAPI()
 
-# For static files (e.g., CSS, JavaScript, images)
+# пути для файликов CSS, JavaScript, images
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Jinja2 templates setup
+# для отображения HTML-шаблонов в приложении Fast API
 templates = Jinja2Templates(directory="templates")
 
-# Database setup
+# подключение к бд
 DATABASE_URL = "sqlite:///./database.db"
+
+# cоздаем движок базы данных
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Настраиваем сессию для работы с базой данных
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# базовый класс для моделей SQLAlchemy
 Base = declarative_base()
 
 
+# маршрут для /index
 @app.get("/index", response_class=HTMLResponse)
 async def about(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
+# маршрут для /bouquets
 @app.get("/bouquets", response_class=HTMLResponse)
 async def bouquets(request: Request):
     return templates.TemplateResponse("bouquets.html", {"request": request})
 
 
+# маршрут для /gifts
 @app.get("/gifts", response_class=HTMLResponse)
 async def gifts(request: Request):
     return templates.TemplateResponse("gifts.html", {"request": request})
 
 
+# маршрут для /contacts
 @app.get("/contacts", response_class=HTMLResponse)
 async def contacts(request: Request):
     return templates.TemplateResponse("contacts.html", {"request": request})
 
 
-# Routes
+# маршрут /, который возвращает HTML-шаблон index.html
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
+# маршрут, который возвращает список букетов из базы данных products.db в формате JSON
 @app.get("/api/products")
 async def get_products():
     conn = sqlite3.connect("products.db")
@@ -62,6 +83,7 @@ async def get_products():
     return JSONResponse(content=[dict(product) for product in products])
 
 
+# маршрут, который возвращает список подарков из базы данных gifts.db в формате JSON
 @app.get("/api/gifts")
 async def get_gifts():
     conn = sqlite3.connect("gifts.db")
@@ -71,6 +93,7 @@ async def get_gifts():
     return JSONResponse(content=[dict(gift) for gift in gifts])
 
 
+# маршрут, который возвращает детали конкретного букета по его id
 @app.get("/api/products/{product_id}")
 async def get_product_details(product_id: int):
     conn = sqlite3.connect("products.db")
@@ -80,10 +103,12 @@ async def get_product_details(product_id: int):
     ).fetchone()
     conn.close()
     if product is None:
+        # генерирует исключение
         raise HTTPException(status_code=404, detail="Product not found")
     return JSONResponse(content=dict(product))
 
 
+# маршрут, который возвращает детали конкретного подарка по его id
 @app.get("/api/gifts/{gift_id}")
 async def get_gift_details(gift_id: int):  # Аргумент совпадает с {gift_id}
     conn = sqlite3.connect("gifts.db")
@@ -95,6 +120,7 @@ async def get_gift_details(gift_id: int):  # Аргумент совпадает
     return JSONResponse(content=dict(gift))
 
 
+# маршрут, который возвращает HTML-шаблон с деталями букета
 @app.get("/product-details/{product_id}", response_class=HTMLResponse)
 async def product_details(request: Request, product_id: int):
     conn = sqlite3.connect("products.db")
@@ -112,6 +138,7 @@ async def product_details(request: Request, product_id: int):
     )
 
 
+# маршрут, который возвращает HTML-шаблон с деталями подарка
 @app.get("/gift-details/{gift_id}", response_class=HTMLResponse)
 async def gifts_details(request: Request, gift_id: int):
     conn = sqlite3.connect("gifts.db")
@@ -127,6 +154,7 @@ async def gifts_details(request: Request, gift_id: int):
     )
 
 
+# запускаем приложение с помощью uvicorn на локальном хосте 127.0.0.1 и порту 5500.
 if __name__ == "__main__":
     import uvicorn
 
